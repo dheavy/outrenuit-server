431691aa40f9f045a9800f607d58af71fe8277ed
Davy Peter Braun <hello@davybraun.com>
2020-04-20 23:28:51 +0200

    Add information about Artefacts' purposes

--- apps/artefacts/apps.py
+++ apps/artefacts/apps.py
@@ -1,3 +1,11 @@
+'''
+Artefacts are results from the inquiry.
+They can be Freudian slips from registered dreams,
+or quick notes taken from ideas while registering dream
+or reviewing them, or symbols highlighted from registered
+texts. They are linked to words or snippets of registered
+texts (a.k.a "labels").
+'''
 from django.apps import AppConfig
 
 
